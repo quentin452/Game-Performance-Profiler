c6a21f9c62b084cd2d4f433c9f615d3cedb12681
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-09-07 23:31:27 +0200

    Update game_performance_profiler.hpp

--- includes/game_performance_profiler.hpp
+++ includes/game_performance_profiler.hpp
@@ -15,15 +15,14 @@
 
 // COPYRIGHT MIT : created by https://github.com/quentin452
 class GamePerformanceProfiler {
-public:
-    GamePerformanceProfiler()
-    {
+  public:
+    GamePerformanceProfiler() {
 #ifdef PERFORMANCE_MONITOR
         realTimeStart = std::chrono::steady_clock::now();
 #endif
     }
 
-private:
+  private:
 #ifdef PERFORMANCE_MONITOR
     std::unordered_map<std::string, std::chrono::steady_clock::time_point> startTimes;
     std::unordered_map<std::string, int64_t> profilingData;
@@ -33,12 +32,11 @@ private:
     std::chrono::steady_clock::time_point realTimeStart;
     std::vector<std::string> warningsIssued;
 #endif
-public:
+  public:
 #ifdef PERFORMANCE_MONITOR
     bool stress_test_enabled = true;
 #endif
-    void start(const std::string& name, const std::string& file, int line, const std::string& customName)
-    {
+    void start(const std::string &name, const std::string &file, int line, const std::string &customName) {
 #ifdef PERFORMANCE_MONITOR
         auto now = std::chrono::steady_clock::now();
         std::string key = customName.empty() ? (name + ":" + file + ":" + std::to_string(line)) : customName;
@@ -51,8 +49,7 @@ public:
 #endif
     }
 
-    void stop(const std::string& name, const std::string& file, int line, const std::string& customName)
-    {
+    void stop(const std::string &name, const std::string &file, int line, const std::string &customName) {
 #ifdef PERFORMANCE_MONITOR
         auto now = std::chrono::steady_clock::now();
         std::string key = customName.empty() ? (name + ":" + file + ":" + std::to_string(line)) : customName;
@@ -65,8 +62,7 @@ public:
                 profilingData[key] += duration;
                 maxTimes[key] = std::max(maxTimes[key], duration);
                 startTimes.erase(it);
-            }
-            else {
+            } else {
                 std::string warningMessage = "Warning: Profiling stopped for a method that was not started: " + key;
                 if (std::find(warningsIssued.begin(), warningsIssued.end(), warningMessage) == warningsIssued.end()) {
                     warningsIssued.push_back(warningMessage);
@@ -76,8 +72,70 @@ public:
 #endif
     }
 
-    void print()
-    {
+    void printWithoutLogMessageAsync(const std::string &filePath) {
+#ifdef PERFORMANCE_MONITOR
+        try {
+            if (profilingData.empty()) {
+                std::ofstream file(filePath, std::ios::app);
+                if (file.is_open()) {
+                    file << "Warning: profilingData is empty." << std::endl;
+                    file.close();
+                }
+                return;
+            }
+
+            double totalProfilingTimeSec = 0;
+            std::vector<std::tuple<std::string, double, double, double, double>> sortedData;
+
+            for (const auto &entry : profilingData) {
+                std::string key = entry.first;
+                double totalTimeSec = entry.second / 1000000.0;
+                double avgTimeSec = totalTimeSec / callCounts.at(key);
+                double maxTimeSec = maxTimes.at(key) / 1000000.0;
+                double combinedAvg = (avgTimeSec + maxTimeSec) / 2.0;
+                sortedData.push_back(std::make_tuple(key, totalTimeSec, avgTimeSec, maxTimeSec, combinedAvg));
+
+                totalProfilingTimeSec += totalTimeSec;
+            }
+
+            std::sort(sortedData.begin(), sortedData.end(), [](const auto &a, const auto &b) {
+                return std::get<1>(b) < std::get<1>(a);
+            });
+
+            auto now = std::chrono::steady_clock::now();
+            auto realTimeElapsed = std::chrono::duration_cast<std::chrono::seconds>(now - realTimeStart).count();
+
+            std::ofstream file(filePath, std::ios::app);
+            if (file.is_open()) {
+                file << "Total Profiling Time: " + std::to_string(totalProfilingTimeSec) + " s, Real Time Elapsed: " + std::to_string(realTimeElapsed) + " s" << std::endl;
+
+                for (const auto &entry : sortedData) {
+                    std::string key = std::get<0>(entry);
+                    double totalTimeSec = std::get<1>(entry);
+                    double avgTimeSec = std::get<2>(entry);
+                    double maxTimeSec = std::get<3>(entry);
+                    std::string logMessage = key + ": " + std::to_string(totalTimeSec) + " s (Total Time), " + std::to_string(avgTimeSec) + " s (Average Time For one call), " + std::to_string(maxTimeSec) + " s (Max Time For one call)" + " With a Total Calls: " + std::to_string(callCounts.at(key));
+                    file << logMessage << std::endl;
+                }
+
+                for (const std::string &warningMessage : warningsIssued) {
+                    file << "Warning: " << warningMessage << std::endl;
+                }
+                warningsIssued.clear();
+
+                file.close();
+            }
+        } catch (const std::exception &e) {
+            std::ofstream file(filePath, std::ios::app);
+            if (file.is_open()) {
+                file << "Error: " << e.what() << std::endl;
+                file.close();
+            }
+        }
+#endif
+    }
+
+    void printUsingLogMessage() {
 #ifdef PERFORMANCE_MONITOR
         try {
             if (profilingData.empty()) {
@@ -89,7 +147,7 @@ public:
             double totalProfilingTimeSec = 0;
             std::vector<std::tuple<std::string, double, double, double, double>> sortedData;
 
-            for (const auto& entry : profilingData) {
+            for (const auto &entry : profilingData) {
                 std::string key = entry.first;
                 double totalTimeSec = entry.second / 1000000.0;
                 double avgTimeSec = totalTimeSec / callCounts.at(key);
@@ -100,40 +158,35 @@ public:
                 totalProfilingTimeSec += totalTimeSec;
             }
 
-            std::sort(sortedData.begin(), sortedData.end(), [](const auto& a, const auto& b) {
+            std::sort(sortedData.begin(), sortedData.end(), [](const auto &a, const auto &b) {
                 return std::get<1>(b) < std::get<1>(a);
             });
 
             auto now = std::chrono::steady_clock::now();
             auto realTimeElapsed = std::chrono::duration_cast<std::chrono::seconds>(now - realTimeStart).count();
 
-            std::string totalLogMessage = "Total Profiling Time: " + std::to_string(totalProfilingTimeSec)
-                + " s, Real Time Elapsed: " + std::to_string(realTimeElapsed) + " s";
+            std::string totalLogMessage = "Total Profiling Time: " + std::to_string(totalProfilingTimeSec) + " s, Real Time Elapsed: " + std::to_string(realTimeElapsed) + " s";
             LoggerThread::GetLoggerThread().logMessageAsync(LogLevel::INFO, __FILE__, __LINE__, totalLogMessage);
 
-            for (const auto& entry : sortedData) {
+            for (const auto &entry : sortedData) {
                 std::string key = std::get<0>(entry);
                 double totalTimeSec = std::get<1>(entry);
                 double avgTimeSec = std::get<2>(entry);
                 double maxTimeSec = std::get<3>(entry);
-                std::string logMessage = key + ": " + std::to_string(totalTimeSec) + " s (Total Time), "
-                    + std::to_string(avgTimeSec) + " s (Average Time For one call), " + std::to_string(maxTimeSec)
-                    + " s (Max Time For one call)" + " With a Total Calls: " + std::to_string(callCounts.at(key));
+                std::string logMessage = key + ": " + std::to_string(totalTimeSec) + " s (Total Time), " + std::to_string(avgTimeSec) + " s (Average Time For one call), " + std::to_string(maxTimeSec) + " s (Max Time For one call)" + " With a Total Calls: " + std::to_string(callCounts.at(key));
                 LoggerThread::GetLoggerThread().logMessageAsync(LogLevel::INFO, __FILE__, __LINE__, logMessage);
             }
-            for (const std::string& warningMessage : warningsIssued) {
+            for (const std::string &warningMessage : warningsIssued) {
                 LoggerThread::GetLoggerThread().logMessageAsync(LogLevel::WARNING, __FILE__, __LINE__, warningMessage);
             }
             warningsIssued.clear();
-        }
-        catch (const std::exception& e) {
+        } catch (const std::exception &e) {
             LoggerThread::GetLoggerThread().logMessageAsync(LogLevel::ERRORING, __FILE__, __LINE__, e.what());
         }
 #endif
     }
 
-    void addData(const std::string& key, int64_t value)
-    {
+    void addData(const std::string &key, int64_t value) {
 #ifdef PERFORMANCE_MONITOR
         std::lock_guard<std::mutex> lock(mtx);
         profilingData[key] += value;
@@ -149,18 +202,19 @@ inline GamePerformanceProfiler gamePerformanceProfiler;
 #define PROFILE_STOP(customName) gamePerformanceProfiler.stop(__FUNCTION__, __FILE__, __LINE__, customName);
 
 class ScopedProfiler {
-public:
-    ScopedProfiler(const std::string& name) : name(name) {
+  public:
+    ScopedProfiler(const std::string &name) : name(name) {
         PROFILE_START(name);
     }
     ~ScopedProfiler() {
         PROFILE_STOP(name);
     }
-private:
+
+  private:
     std::string name;
 };
 
-#define PROFILE_SCOPED(customName)  ScopedProfiler profiler(customName);
+#define PROFILE_SCOPED(customName) ScopedProfiler profiler(customName);
 #else
 #define PROFILE_START(customName)
 #define PROFILE_STOP(customName)
